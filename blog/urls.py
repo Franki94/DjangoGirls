97c62f79e96c8470c3ec603f2fce1146c55fea9a
becker oliver utia lopez <becker_utia_123@hotmail.com>
2018-04-13 23:27:14 -0500

    First view

--- blog/urls.py
+++ blog/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from blog import views
 
 #path(route, view, kwargs=None, name=None)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    # re_path(r'^', views.post_list, name='post_list'),#Cuando queremos usar expresiones regulares
+    path('', views.post_list, name='post_list'),
+
 ]
